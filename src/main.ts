3fab69b2a05a2208382b13a5eb45e08681a094be
tobiadiks <tobiadiks@gmail.com>
2021-12-15 22:01:59 +0100

    port update

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
     methods: 'GET, PUT, POST, DELETE',
     allowedHeaders: 'Content-Type, Authorization',
 });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();

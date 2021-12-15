0cffd02d95e87b389495e872d9158005f8ee7cc2
tobiadiks <tobiadiks@gmail.com>
2021-12-15 21:34:52 +0100

    update env

--- src/db.config.ts
+++ src/db.config.ts
@@ -3,14 +3,19 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export function DbConfig(): TypeOrmModuleOptions {
   return {
     type: 'postgres',
-    host: process.env.host,
+    host:
+      process.env.NODE_ENV == `production`
+        ? `ec2-184-73-25-2.compute-1.amazonaws.com`
+        : 'localhost',
     port: 5432,
-    username: process.env.username,
-    password: process.env.password,
-    database: process.env.database,
+    username:
+      process.env.NODE_ENV == `production` ? `vjhxvpfqrswzkt` : 'postgres',
+    password:
+      process.env.NODE_ENV == `production`
+        ? `e1f0e2d6bdd0ed82629d7bd96994aa9baf5d308ad26e161447d2fb7bae0bdeb6`
+        : '1234',
+    database: process.env.NODE_ENV == `production` ? `df0qjgqppjpga0` : 'fpp',
     synchronize: true,
     autoLoadEntities: true,
-    logging: false,
-    dropSchema: false,
   };
 }
